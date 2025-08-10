32edfad7e62bedf0cdd538bdb1dec99725dfe010
sergiorueda9090 <sergiorueda90@hotmail.com>
2025-08-09 19:39:24 -0500

    commit

--- src/constants/constantGlogal.js
+++ src/constants/constantGlogal.js
@@ -1,3 +1,4 @@
 //export const URL  = 'https://cda.sergiodevsolutions.com';
 //export const URL  = 'https://backend.movilidad2a.com';
-export const dominio = 'http://127.0.0.1:8000/';
+//export const dominio = 'http://127.0.0.1:8000/';
+export const dominio = 'http://backend.omarruedaorbe.com/';
