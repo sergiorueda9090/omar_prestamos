1fef4acd0f10950c62ee3660d1c8ae15ac05fc81
sergio rueda <sergiorueda90@hotmail.com>
2026-03-16 21:07:22 -0500

    cambios

--- src/constants/constantGlogal.js
+++ src/constants/constantGlogal.js
@@ -1,2 +1,2 @@
-export const dominio = 'http://127.0.0.1:8000/';
-//export const dominio = 'https://backend.omarruedaorbe.com/';
+//export const dominio = 'http://127.0.0.1:8000/';
+export const dominio = 'https://backend.omarruedaorbe.com/';
